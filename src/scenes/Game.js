cccdda2bad7c9fe5dbbd97a6d3c4ac10b5c2e635
Bianca Ercole <127907362+biancuercole@users.noreply.github.com>
2023-08-12 17:29:00 -0300

    funcion de collider pala y pelota arreglado

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,11 +5,11 @@ export default class Game extends Phaser.Scene {
   }
 
   init() {
-
+    this.score == 1;
   }
 
   create() {
-    console.log("hola");
+    console.log("prueba");
 
     this.ball = this.physics.add.sprite(400, 300, "ball").setScale(0.20)
     .setCollideWorldBounds(true)
@@ -18,9 +18,10 @@ export default class Game extends Phaser.Scene {
     .setVelocity(100, -200);
 
     this.bat = this.physics.add.sprite(600, 400, "platform").setScale(0.26)
-    .setCollideWorldBounds(true);
+    .setCollideWorldBounds(true)
+    .setBounce(1, 1);
 
-    this.physics.add.collider(this.bat, this.ball);
+    this.physics.add.collider(this.ball, this.bat, this.points, null, this);
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
@@ -32,6 +33,7 @@ export default class Game extends Phaser.Scene {
       this.bat.setVelocityX(100)
     } else {
       this.bat.setVelocityX(0)
+      this.bat.body.allowGravity = false;
     }
     if(this.cursors.up.isDown) {
       this.bat.setVelocityY(-100)
@@ -39,6 +41,18 @@ export default class Game extends Phaser.Scene {
       this.bat.setVelocityY(100)
     } else {
       this.bat.setVelocityY(0)
+      this.bat.body.allowGravity = false;
     }
+  }
+
+  points (ball, bat) {
+    this.score += 1;
+    console.log(this.score);
+    if (ball.body.velocity.x > 0) {
+      ball.setVelocity(100, -200);
+    } else {
+      ball.setVelocity(-100, -200);
+    }
+    //this.ball.body.setVelocitY(100, -200);
   }
 }
